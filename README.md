9f1e00784dc066afcdcf2309804867264b82d92c
Gilli <mjg.py3@gmail.com>
2014-02-15 08:54:20 -0500

    Started doing some poc of webdriver

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Library Code Ripper
 Dependencies
 -----------
 
+python 1.7
+
 pip
 ```sudo apt-get install python-pip```
 
 selenium
 ```sudo apt-get install selenium```
 
+phantomjs
+```sudo apt-get install phantomjs```
--- lib_code_ripper.py
+++ lib_code_ripper.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+
+DEBUG = True
+
+if __name__ == '__main__':
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+
+    driver = webdriver.PhantomJS() if not DEBUG else webdriver.Firefox()
+
+    driver.get("http://catalog.loc.gov/")
+    search_box = driver.find_element_by_name("Search_Arg")
+    search_box.send_keys("foobar")
+    search_box.send_keys(Keys.RETURN)
